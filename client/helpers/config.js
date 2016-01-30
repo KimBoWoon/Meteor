d7d6a1f1e3c73bc4fd097509d6aa12ba48075801
BW <bw@localhost.localdomain>
2016-01-30 17:26:18 +0900

    POST 등록

---
+++ client/helpers/config.js
@@ -0,0 +1,7 @@
+/**
+ * Created by bw on 16. 1. 30.
+ */
+
+Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_ONLY'
+});
---
+++ client/templates/application/layout.js
@@ -0,0 +1,7 @@
+/**
+ * Created by bw on 16. 1. 30.
+ */
+
+Template.layout.helpers({
+    pageTitle: function() { return Session.get('pageTitle'); }
+});
